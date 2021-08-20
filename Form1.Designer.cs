fef0a3e317c80993e491091335cb61f288f47a0e
TutuMbs <84512809+TutuMbs@users.noreply.github.com>
2021-08-20 11:42:19 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace at2tri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblpeso = new System.Windows.Forms.Label();
+            this.lblaltura = new System.Windows.Forms.Label();
+            this.btexibir = new System.Windows.Forms.Button();
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.txtaltura = new System.Windows.Forms.TextBox();
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblpeso
+            // 
+            this.lblpeso.AutoSize = true;
+            this.lblpeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpeso.Location = new System.Drawing.Point(12, 116);
+            this.lblpeso.Name = "lblpeso";
+            this.lblpeso.Size = new System.Drawing.Size(265, 13);
+            this.lblpeso.TabIndex = 0;
+            this.lblpeso.Text = "Digite o peso em toneladas do seu caminhão:";
+            // 
+            // lblaltura
+            // 
+            this.lblaltura.AutoSize = true;
+            this.lblaltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblaltura.Location = new System.Drawing.Point(12, 177);
+            this.lblaltura.Name = "lblaltura";
+            this.lblaltura.Size = new System.Drawing.Size(252, 13);
+            this.lblaltura.TabIndex = 1;
+            this.lblaltura.Text = "Digite o altura em metros do seu caminhão:";
+            // 
+            // btexibir
+            // 
+            this.btexibir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btexibir.Location = new System.Drawing.Point(182, 232);
+            this.btexibir.Name = "btexibir";
+            this.btexibir.Size = new System.Drawing.Size(178, 31);
+            this.btexibir.TabIndex = 2;
+            this.btexibir.Text = "Exibir";
+            this.btexibir.UseVisualStyleBackColor = true;
+            this.btexibir.Click += new System.EventHandler(this.btexibir_Click);
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Location = new System.Drawing.Point(283, 114);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(100, 20);
+            this.txtpeso.TabIndex = 3;
+            // 
+            // txtaltura
+            // 
+            this.txtaltura.Location = new System.Drawing.Point(283, 174);
+            this.txtaltura.Name = "txtaltura";
+            this.txtaltura.Size = new System.Drawing.Size(100, 20);
+            this.txtaltura.TabIndex = 4;
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(-4, -2);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(525, 67);
+            this.lbltitulo.TabIndex = 5;
+            this.lbltitulo.Text = "Restrição de trafego";
+            this.lbltitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(517, 305);
+            this.Controls.Add(this.lbltitulo);
+            this.Controls.Add(this.txtaltura);
+            this.Controls.Add(this.txtpeso);
+            this.Controls.Add(this.btexibir);
+            this.Controls.Add(this.lblaltura);
+            this.Controls.Add(this.lblpeso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblpeso;
+        private System.Windows.Forms.Label lblaltura;
+        private System.Windows.Forms.Button btexibir;
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.TextBox txtaltura;
+        private System.Windows.Forms.Label lbltitulo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace at2tri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btexibir_Click(object sender, EventArgs e)
+        {
+            double peso = 0, altura = 0;
+
+
+
+            if (double.TryParse(txtpeso.Text, out peso) == false)
+            {
+                MessageBox.Show("Digite um valor valido referente ao peso do seu caminhão", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtpeso.Clear();
+                txtpeso.Focus();
+                return;
+            }
+            else if (double.TryParse(txtaltura.Text, out altura) == false)
+            {
+                MessageBox.Show("Digite um valor valido referente a altura do seu caminhão", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtaltura.Clear();
+                txtaltura.Focus();
+                return;
+            }
+
+            else if (altura >5)
+            {
+                MessageBox.Show("A altura do seu caminhão não deixa você passar em nenhuma ponte", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else if (peso >= 20 && altura >= 8)
+            {
+                MessageBox.Show("Tente diminuir o peso do seu caminhão ele tem restrições em todas.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else if (peso >= 16 && altura >= 5)
+            {
+                MessageBox.Show("As pontes permitidas para o seu caminhao são as seguintes pontes:Ponte Foguete, Ponte CDA, Ponte Specter, Ponte Scooby e Ponte Cabral.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+            }
+            else if (peso >= 14 && altura >= 4.5)
+            {
+                MessageBox.Show("As pontes permitidas para o seu caminhao são as seguintes pontes:Ponte Outer, Ponte Ney, Ponte Paris, Ponte Mbappe, Ponte Garfield, Ponte Suits, Ponte Fagner, Ponte Jô, Ponte Alberto, Ponte Santos, Ponte Ordem, Ponte Rodrigues, Ponte Fernandes, Ponte Marcio, Ponte horizonte, Ponte Foguete, Ponte CDA, Ponte Specter, Ponte Scooby e Ponte Cabral.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+            }
+            else if (peso >= 13.5 && altura >= 4)
+            {
+                MessageBox.Show("As pontes permitidas para o seu caminhao são as seguintes pontes:Ponte Suits, Ponte Fagner, Ponte Jô, Ponte Alberto, Ponte Santos, Ponte Ordem, Ponte Rodrigues, Ponte Fernandes, Ponte Marcio, Ponte horizonte, Ponte Foguete, Ponte CDA, Ponte Specter, Ponte Scooby e Ponte Cabral.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+            }
+            else if (peso >= 12 && altura >= 3.5)
+            {
+                MessageBox.Show("As pontes permitidas para o seu caminhao são as seguintes pontes:Ponte Ordem, Ponte Rodrigues, Ponte Fernandes, Ponte Marcio, Ponte horizonte, Ponte Foguete, Ponte CDA, Ponte Specter, Ponte Scooby e Ponte Cabral.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+            }
+            else if (peso >= 10 && altura >= 3)
+            {
+                MessageBox.Show("As pontes permitidas para o seu caminhao são as seguintes pontes:Ponte Foguete, Ponte CDA, Ponte Specter, Ponte Scooby e Ponte Cabral.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+            }
+            else if (peso <= 10 && altura <= 3)
+            {
+                MessageBox.Show("O seu caminhão não tem restrição em nenhuma pontes", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+
+            }
+        }
+    }
+}
